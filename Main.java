6d4a516ca9280b140711c1c3611e8a24a914eb97
Tim Johnson <pavementii@gmail.com>
2017-10-13 00:55:41 -0400

    Added Assignment 2 files via upload
    Pre-work Assignment 2: Difference between dates

--- Main.java
+++ Main.java
@@ -1,35 +1,58 @@
 package com.robinsondetroit;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        StringTheory trial = new StringTheory();
 
-        System.out.println("Please enter two integer numbers with the same number of digits.");
+        int year1;
+        int month1;
+        int dayOfMonth1;
 
-        System.out.println("First number: ");
-        String var1 = sc.nextLine();
+        int year2;
+        int month2;
+        int dayOfMonth2;
 
-        System.out.println("Second number: ");
-        String var2 = sc.nextLine();
+        System.out.println("\nFor my next trick, I will tell you the duration between two dates in years, months, and days.");
+        System.out.println("Please enter the first date by entering the YEAR");
+        year1 = sc.nextInt();
 
-        while (!trial.isValidInput(var1, var2)) {
-            System.out.println("\nSorry the two entries either are not the same length or they aren't numbers. \nPlease try again.");
-            System.out.println("\nFirst number: ");
-            var1 = sc.nextLine();
+        System.out.println("Next enter the MONTH of the first date:");
+        month1 = sc.nextInt();
 
-            System.out.println("Second number: ");
-            var2 = sc.nextLine();
-        }
+        System.out.println("And now enter the DAY of the month of the first date:");
+        dayOfMonth1 = sc.nextInt();
 
-        System.out.println("\nI will now see if each corresponding place in the two numbers sums to the same total.\n\n");
-        System.out.println(trial.passesTest(var1, var2));
-        }
+        System.out.println("\nNow let's do the same for the second date. Please enter the YEAR:");
+        year2 = sc.nextInt();
+
+        System.out.println("Now the MONTH of the second date:");
+        month2 = sc.nextInt();
+
+        System.out.println("And finally, the DAY of the month of the second date:");
+        dayOfMonth2 = sc.nextInt();
+
+        LocalDate firstDate = LocalDate.of(year1, month1, dayOfMonth1);
+        LocalDate secondDate = LocalDate.of(year2, month2, dayOfMonth2);
+
+        long daysElapsed = ChronoUnit.DAYS.between(firstDate, secondDate);
+        long monthsElapsed = ChronoUnit.MONTHS.between(firstDate, secondDate);
+        long yearsElapsed = ChronoUnit.YEARS.between(firstDate, secondDate);
+
+        System.out.println("\nAnd the difference between the two dates is.... [drumroll please]...");
+        System.out.println(daysElapsed + " days");
+        System.out.println(monthsElapsed + " months");
+        System.out.println(yearsElapsed + " years\n");
+
+        System.out.println("This is equivolent to " + yearsElapsed + " years, " + (monthsElapsed % 12) + " months, and " +
+                (daysElapsed % 365 % 30) + " days!!!");
     }
 
-
-
+}
